946630ede4ea9bacf9219b5137c9206674a4ea4e
Daniel Barragan <daniel.barragan@alertlogic.com>
2017-02-11 12:52:52 -0500

    add templates and attributes example

---
+++ 07_cookbook_templates/cookbooks/apache/attributes/default.rb
@@ -0,0 +1,4 @@
+#<> Database connection parameters
+default[:db][:ip] = '192.168.131.101'
+default[:db][:user] = 'icesi'
+default[:db][:pass] = '12345'
---
+++ 07_cookbook_templates/cookbooks/mysql/attributes/default.rb
@@ -0,0 +1,4 @@
+#<> Database connection parameters
+default[:wb][:ip] = '192.168.131.100'
+default[:db][:user] = 'icesi'
+default[:db][:pass] = '12345'
